ddb006423b7ba094145c4b4bb78a148d6c900faf
Andreas Marsh <marsh.andreas.s@gmail.com>
2021-02-22 16:08:12 -0500

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Andreas Marsh on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
